9204a32f31525450b6f77f43b243a5f8dd65a86f
Kishan Kadam <kishanskadam@gmail.com>
2022-01-26 21:29:59 +0530

    unit test project added

---
+++ InventoryUnitTest/InventoryMasterUnitTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Xunit;
+using Inventory.Services;
+using Inventory.Entities;
+
+namespace InventoryUnitTest
+{
+    public class InventoryMasterUnitTest
+    {
+        private readonly InventoryMasterService _service;
+        
+        public InventoryMasterUnitTest()
+        {
+            _service = new InventoryMasterService();
+        }
+
+        [Fact]
+        public void GetInventoryMastersTest()
+        {
+            var okResult = _service.GetInventoryMasters();
+            Assert.IsType<List<InventoryMaster>>(okResult as List<InventoryMaster>);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        public void GetInventoryMasterItemTest(int id)
+        {
+            var okResult = _service.GetInventoryMasterItem(id);
+            Assert.IsType<InventoryMaster>(okResult as InventoryMaster);
+        }
+    }
+}
